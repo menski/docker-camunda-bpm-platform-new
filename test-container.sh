783c91f3c0498990f09821ad1641da79e7cbbdfa
Sebastian Menski <sebastian@menski.org>
2015-11-21 02:05:39 +0100

    chore(test): improvements

--- test-container.sh
+++ test-container.sh
@@ -4,26 +4,26 @@ function check_container_running {
     local container=${1:-camunda}
     local running=$(docker inspect --format="{{ .State.Running }}" $container)
 
-    echo "$container: Check container state"
+    echo -n "status: Check container $container state"
 
-    [ "$running" != "true" ] && (echo "$container: Container is not running"; return 1)
+    [ "$running" != "true" ] && (echo -e "\033[2K\rstatus: Container $container is not running (abort)"; return 1)
 
-    echo "$container: Container is running"
+    echo -e "\033[2K\rstatus: Container $container is running"
 }
 
 function wait_for_deployment {
     local retries=${1:-20}
     local wait_time=${2:-3}
 
+    echo -n "deploy: Grep log for deployment summary"
     for retry in $(seq $retries); do
-        echo "$retry: Grep log for deployment summary"
         docker logs camunda 2>&1 | grep -q "ENGINE-08023 Deployment summary for process archive 'camunda-invoice'" && \
-            echo "$retry: Deployment summary found" && return 0
+            echo -e "\033[2K\rdeploy: Deployment summary found ($retry/$retries)" && return 0
         if [ $retry -lt $retries ]; then
-            echo "$retry: Deployment summary not found will wait for $wait_time seconds and retry ($(expr $retries - $retry) retries left)"
+            echo -en "\033[2K\rdeploy: Deployment summary not found will wait for $wait_time seconds and retry ($retry/$retries)"
             sleep $wait_time
         else
-            echo "$retry: Deployment summary not found (abort)"
+            echo -e "\033[2K\rdeploy: Deployment summary not found (abort)"
         fi
     done
 
@@ -34,12 +34,12 @@ function test_login {
     local app=${1:-cockpit}
     local retries=${2:-3}
     local wait_time=${3:-3}
-    echo "$app: Test login"
+    echo -n "webapp: Test $app login"
 
-    curl --fail -s --retry $retries --retry-delay $wait_time  --data 'username=demo&password=demo' -D- -o/dev/null http://localhost:8080/camunda/api/admin/auth/user/default/login/${app} || \
-        (echo "$app: Login failed (abort)"; return 3)
+    curl --fail -s --retry $retries --retry-delay $wait_time  --data 'username=demo&password=demo' -Dheader.log -o/dev/null http://localhost:8080/camunda/api/admin/auth/user/default/login/${app} || \
+        (echo -e "\033[2K\rwebapp: Login $app failed (abort)"; cat header.log; return 3)
 
-    echo "$app: Login successful"
+    echo -e "\033[2K\rwebapp: Login $app successful"
 }
 
 # check container state
